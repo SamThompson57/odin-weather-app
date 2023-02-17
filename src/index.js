ff0fe822b09debda512c6fbcf55c6fa22d64a68b
Pidgicle <s_thomo@hotmail.co.uk>
2023-02-17 10:39:02 +0000

    Search can be made with postcode and by name next need to convert the data and add customisations for units.

--- src/index.js
+++ src/index.js
@@ -15,19 +15,54 @@ const submit = document.getElementById('submit')
 submit.onclick = () => getWeather()
 
 const city = document.getElementById('city')
+const local = document.getElementById('location')
+const temp = document.getElementById('temp')
+const desc = document.getElementById('desc')
+const feels = document.getElementById('feels')
+const humid = document.getElementById('humidity')
+const wind = document.getElementById('wind')
+
+let lat = 0
+let lon = 0
+
+
+function containsNumbers(str) {
+    if(/\d/.test(str)){
+        return `http://api.openweathermap.org/geo/1.0/zip?zip=${city.value},GB&appid=${api}`
+    } else {
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${city.value.replace(" ", "")}&appid=${api}`
+    }
+  }
 
 async function getWeather(){
-    
-    //Gets the long and lat data from desired location
-    const locResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city.value}&appid=${api}`, {mode: 'cors'}) 
-    const locData = await locResponse.json();
 
-    const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${locData[0].lat}&lon=${locData[0].lon}&appid=${api}`, {mode: 'cors'})
+    city.value = city.value.split(' ').join('-')
+    //Gets the long and lat data from desired location
+    const locResponse = await fetch(containsNumbers(city.value), {mode: 'cors'})
+    
+    const locData = await locResponse.json();
+    console.log(locData)
+
+    if(/\d/.test(city.value)){
+        lat = locData.lat
+        lon = locData.lon
+    } else {
+        lat = locData[0].lat
+        lon = locData[0].lon
+    }
+    console.log(`LAT:${lat} LON:${lon}`)
+    const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}`, {mode: 'cors'})
     const weatherData = await weatherResponse.json();
 
     console.log(weatherData)
-
-    
+    local.textContent = `${weatherData.name}, ${weatherData.sys.country}`
+    temp.textContent = `${weatherData.main.temp}` //Default is Kelvin
+    desc.textContent = `${weatherData.weather[0].description}`
+    feels.textContent = `Feels Like: ${weatherData.main.feels_like}`
+    humid.textContent = `Humidity: ${weatherData.main.humidity}`
+    wind.textContent = `Wind: ${weatherData.wind.speed} M/Sec`
 }
+
+
 
 getWeather();
